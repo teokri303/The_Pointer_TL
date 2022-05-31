c598606bb0cce7c4138cc4f461e3245f71d7af56
dairuinz <45816589+dairuinz@users.noreply.github.com>
2022-05-31 17:09:24 +0300

    Add files via upload

---
+++ Event.py
@@ -0,0 +1,134 @@
+#
+#Event Class
+#
+class Event:
+    #event info
+    _id = 0
+    _name = ""
+    _points_g = 0
+    _points_r = 0
+    _cap = 0
+    _prv = False
+    _starts = dt.datetime.today()
+    _ends = dt.datetime.today()
+    #usr info
+    _creator = None
+    _participate = []
+
+    def __init__(self,id = 0,name = "",points_g = 0,points_r = 0,cap = 0,prv = False,creator = None,participate = [],starts = dt.datetime.today(),ends = dt.datetime.today()):
+        #event info
+        self._id = id
+        self._name = name
+        self._points_g = points_g
+        self._points_r = points_r
+        self._cap = cap
+        self._prv = prv
+        self._starts = starts
+        self._ends = ends
+        #usr info
+        self._creator = creator
+        self._participate = participate
+
+    #event info
+    def set_name(self,name = ""):
+        self._name = name
+    def points(self,points_g = 0,points_r = 0):
+        self.points_g = points_g
+        self.points_r = points_r
+    def set_cap(self,cap = 0):
+        self._cap = cap
+    def set_counter(self,counter):
+        self._counter = counter
+    def set_points_r(self,points_r):
+        self._points_r = points_r
+    def set_points_g(self,points_g):
+        self._points_g = points_g
+    def set_prv(self, prv = False):
+        self._prv = prv
+    #user info
+    def set_creator(self,creator = None):
+        self._creator = creator
+    def set_usrs(self,participate = []):
+        self._participate = participate
+    #date info
+    def set_starts(self,starts):
+        self._starts = startsdef
+    def set_ends(self,ends):
+        self._ends = ends
+    #
+    #getter
+    #
+    #event info
+    def get_id(self):
+        return self._id
+    def get_name(self):
+        return self._name
+    def get_points(self):
+        return (self._points_g,self._points_r)
+    def get_location(self):
+        return self._location
+    def get_cap(self):
+        return self._cap
+    def get_counter(self):
+        return self._counter
+    def get_points_r(self):
+        return self._points_r
+    def get_points_g(self):
+        return self._points_g
+    def get_prv(self):
+        return self._prv
+    def get_prv_string(self):
+        if self._prv == 1:
+            return "Private Event"
+        else:
+            return "Public Event"
+    #date info
+    def get_starts(self):
+        return self._starts
+    def get_ends(self):
+        return self._ends
+    #user info
+    def get_creator(self):
+        return self._creator
+    def get_users(self):
+        return self._participate
+    def get_num_users(self):
+        return len(self._participate)
+    #
+    #event string
+    #
+    def toString(self):
+        return "Name : "+self._name + " Points Required : " +str(self._points_r)+" \nMax Capacity : "+str(self._cap)+" Number of People : "+str(len(self._participate))
+    #
+    #event string
+    #
+    #
+    #event dict
+    #
+    def DictInfo(self):
+        return {
+        "id" : self._id,
+        "name" : self._name ,
+        "location" : self._location ,
+        "points_gained" : self._points_g ,
+        "points_required" : self._points_r ,
+        "capacity" : self._cap ,
+        "private" : self._prv,
+        "starts" : str(self._starts ),#paizei na 8elei moda
+        "ends" : str(self._ends) ,
+        #usr info
+        "creator" : self._creator ,
+        "participate" : self._participate, #sigoura 8elei moda
+        "participators_num" : self.get_num_users()
+        }
+
+    def add_usr(self,usr):
+        self._participate.append(usr)
+    def user_to_participate(self,usr):# an epistrepsei True mporei na mpei alliws oxi
+        if self._cap == len(self._participate):
+            return False
+        for i in self._participate:
+            if i.get_id() == usr.get_id():
+                return False
+        #den einai mesa kai capacity < max
+        return True
--- User.py
+++ User.py
@@ -1,71 +1,76 @@
-
-#
-#User Class
-#
 class User:
     #user info
     _id = None
-    _online = None
     _username = None
     _email = None
-    _coords = None
-    _points = 0
-    _num_of_friends = 0
-    _at_event = False
-    #_my_friends = []#edw 8a periexontai ontothtes user
-    def __init__(self,username = "",email = "", coords = "",points = 0,online = 0,my_friends = None,id = None,num_of_friends=0):
-        #user info
+    _lon = None
+    _lat = None
+
+    def __init__(self,id = None,username = "",email = "", lon = 0.00,lat = 0.00):
         self._id = id
         self._username = username
         self._email = email
-        self._coords = coords
-        self._points = points
-        self._online = online
-        self._num_of_friends = num_of_friends
-    #    self._my_friends = my_friends
-    #
-    #setter
-    #
+        self._lon = lon
+        self._lat = lat
     #user info
     def set_username(self,username):
         self._username = username
     def set_email(self,email = ""):
         self._email = email
-    def set_coords(self, coords):
-        self._coords = coords
+    def set_lon(self,lon):
+        self._lon = lon
+    def set_lat(self,lat):
+        self._lat = lat
+
+    def get_id(self):
+        return self._id
+    def get_coords(self):
+        return {
+            "lat" : self._lat,
+            "lon" : self._lon
+        }
+    def get_username(self):
+        return self._username
+    def get_email(self):
+        return self._email
+
+class RegularUser(User):
+    _online = None
+    _points = 0
+    _pcoins = 0
+    _num_of_friends = 0
+    _at_event = False
+    _finished_events = 0
+
+    def __init__(self,id = None,username = "",email = "", lon = 0.00,lat = 0.00,points = 0,pcoins = 0,online = 0,num_of_friends=0,finished_events = 0):
+        #user info
+        super(User,self).__init__(id,username,email,lon,lat)
+        self._online = online
+        self._points = points
+        self._pcoins = pcoins
+        self._num_of_friends = num_of_friends
+        self._finished_events = finished_events
+    #
+    #setter
+    #
     def set_points(self, points):
         self._points=points
+    def set_pcoins(self,pcoins):
+        self._pcoins = pcoins
     def set_online(self,online):
         self._online = online
     def set_num_of_friends(self,nm):
         self._num_of_friends = nm
-    def point_sum(self,sum):
-        self._points+=sum
     def set_at_event(self,at_event = 1):
         if at_event == 1:
             self._at_event = True
         else:
             self._at_event = False
-    #def set_friends(self,my_friends):
-    #    self._my_friends=my_friends
-    #
-    #getter
-    #
-    #user info
-    def get_id(self):
-        return self._id
+
     def get_points(self):
         return self._points
-    def get_username(self):
-        return self._username
-    def get_email(self):
-        return self._email
-    def get_coords(self):
-        return self._coords
-    def get_lat(self):
-        return self._coords["lat"]
-    def get_lon(self):
-        return self._coords["lon"]
+    def get_pcoins(self):
+        return self._pcoins
     def get_online(self):
         return self._online
     def get_online_string(self):
@@ -80,25 +85,28 @@ class User:
             return "At Event"
         else:
             return "Not At Event"
-    #def get_friends(self):
-    #    return self._my_friends
-    #def get_many_friends(self):
-    #    return len(self._my_friends)
-    def user_dict(self):
+    def finished_event(self):
+        self._finished_events += 1
+        if self._finished_events == 3:
+            self._pcoins += 1
+            self._finished_events = 0
+        else:
+            pass
+    def point_sum(self,sum):
+        self._points+=sum
+    def pcoin_sum(self,p):
+        self._pcoins += p
+
+    def DictInfo(self):
         mdict = {
         "id" : self._id,
         "username": self._username,
         "email": self._email,
-        "coords" : {
-            "lat" : self._coords["lat"],
-            "lon" : self._coords["lon"]
-            },
+        "coords" : self.get_coords(),
         "points" : self._points,
         "online" : self._online
         }
         return mdict
+
     def StringInfo(self):
-        return 'Username : ' + self._username + '\n Friends : ' + str(self._num_of_friends)
-#
-#Gia User
-#
+        return 'Username : ' + self._username + '\nFriends : ' + str(self._num_of_friends) + '\nState : ' + self.get_online_string() + '\n'+self.get_at_event()
